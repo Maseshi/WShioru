3e6851e18b0ae047434a1dcc6cc30c076d61f9f4
Maseshi <dermhioasw123@gmail.com>
2022-05-04 01:36:28 +0700

    📄 Add missing information and add translation.

--- src/pages/Home/Survey.jsx
+++ src/pages/Home/Survey.jsx
@@ -1,6 +1,30 @@
+import { useState, useEffect } from 'react';
+import { getDatabase, ref, onValue, set } from 'firebase/database'
 import Waves from '../../components/Waves/index'
 
-export default function Survey() {
+export default function Survey(props) {
+    const [survey, setSurvey] = useState()
+
+    const translate = props.language
+
+    useEffect(() => {
+        const childRef = ref(getDatabase(), 'Shioru/data/survey')
+        onValue(childRef, snapshot => {
+            if (snapshot.exists()) {
+                const data = snapshot.val()
+
+                setSurvey(data)
+            } else {
+                set(childRef, {
+                    "commands": 0,
+                    "members": 0,
+                    "servers": 0,
+                    "working": 0
+                })
+            }
+        })
+    }, [])
+
     return (
         <>
             <Waves position="top" r={173} g={216} b={230} />
@@ -8,23 +32,23 @@ export default function Survey() {
                 <div className="row">
                     <div className="col-md-3">
                         <i className="bi bi-code-slash"></i>
-                        <h3>-:-</h3>
-                        <span>คำสั่ง</span>
+                        <h3>{survey ? survey.commands : "-:-"}</h3>
+                        <span>{translate.pages.home.survey_commands}</span>
                     </div>
                     <div className="col-md-3">
                         <i className="bi bi-server"></i>
-                        <h3>-:-</h3>
-                        <span>เซิร์ฟเวอร์</span>
+                        <h3>{survey ? survey.servers : "-:-"}</h3>
+                        <span>{translate.pages.home.survey_servers}</span>
                     </div>
                     <div className="col-md-3">
                         <i className="bi bi-people"></i>
-                        <h3>-:-</h3>
-                        <span>สมาชิก</span>
+                        <h3>{survey ? survey.members : "-:-"}</h3>
+                        <span>{translate.pages.home.survey_members}</span>
                     </div>
                     <div className="col-md-3">
                         <i className="bi bi-check2-circle"></i>
-                        <h3>-:-</h3>
-                        <span>ทำงานแล้ว</span>
+                        <h3>{survey ? survey.working : "-:-"}</h3>
+                        <span>{translate.pages.home.survey_worked}</span>
                     </div>
                 </div>
             </section>
