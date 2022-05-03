28d3338deb439a31b22ca784b2b40d40bc5974b2
Maseshi <dermhioasw123@gmail.com>
2022-05-04 01:25:18 +0700

    🍪 Added localization and changed the time for deleting cookies.

--- src/components/CookieAccept/index.jsx
+++ src/components/CookieAccept/index.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
-import { setCookie } from '../../utils/functions/setCookie';
-import { getCookie } from '../../utils/functions/getCookie';
+import { setCookie } from '../../utils/functions/setCookie'
+import { getCookie } from '../../utils/functions/getCookie'
 
 import './style.css'
 
 export default function CookieAccept() {
     const [accepted, setAccepted] = useState(true)
+
+    const language = getCookie('languageSelect') || window.navigator.userLanguage || window.navigator.language
+    const translate = require('../../languages/' + language + '.json')
 
     useEffect(() => {
         if (!getCookie('acceptCookie')) setAccepted(false)
@@ -19,11 +22,11 @@ export default function CookieAccept() {
                     <h1 className="m-0">🍪</h1>
                 </div>
                 <div className="align-self-center p-1">
-                    <p className="m-0">เราใช้คุกกี้ของบุคคลที่สามเพื่อปรับแต่งเนื้อหา โฆษณา และวิเคราะห์การเข้าชมเว็บไซต์</p>
-                    <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&hl=th" target="_blank" rel="noreferrer">เรียนรู้เพิ่มเติม</a>
+                    <p className="m-0">{translate.components.cookieAccept.we_use_cookie}</p>
+                    <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&hl=th" target="_blank" rel="noreferrer">{translate.components.cookieAccept.learn_more}</a>
                 </div>
                 <div className="align-self-center p-1">
-                    <button type="button" className="btn btn-primary btn-sm" onClick={() => setCookie('acceptCookie', '1', 7)} data-bs-dismiss="toast">ตกลง</button>
+                    <button type="button" className="btn btn-primary btn-sm" onClick={() => setCookie('acceptCookie', '1', 30)} data-bs-dismiss="toast">{translate.components.cookieAccept.accept}</button>
                 </div>
             </div>
         </div>
