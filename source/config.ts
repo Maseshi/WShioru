fe4d1e14958885e3c209fa62b4904272f36e7481
Chaiwat Suwannarat <dermhioasw123@gmail.com>
2025-05-16 18:39:53 +0700

    🔧 Update configuration.

---
+++ source/config.ts
@@ -0,0 +1,16 @@
+const config = {
+  firebase: {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  },
+  recaptcha: import.meta.env.VITE_RECAPTCHA,
+  statuspageId: import.meta.env.VITE_STATUSPAGE_ID,
+};
+
+export default config;
